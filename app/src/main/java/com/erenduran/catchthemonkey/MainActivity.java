c4d1c41afb353badda4eefad7b2414bb819d6e2c
TOSHIBA <57902145+eduran2412@users.noreply.github.com>
2021-09-15 23:39:55 +0300

    array created for images

--- app/src/main/java/com/erenduran/catchthemonkey/MainActivity.java
+++ app/src/main/java/com/erenduran/catchthemonkey/MainActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView scoreText;
     TextView timeText;
     int score;
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +32,23 @@ public class MainActivity extends AppCompatActivity {
 
         timeText = (TextView) findViewById(R.id.timeText);
         scoreText = (TextView) findViewById(R.id.scoreText);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
+        imageArray = new ImageView[] {imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9};
+
+        hideImages();
 
         score = 0;
 
-        new CountDownTimer(10000, 1000) {
+        new CountDownTimer(12000, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
@@ -41,6 +65,13 @@ public class MainActivity extends AppCompatActivity {
     public void increaseScore (View view){
         score++;
         scoreText.setText("Score: " +score);
+
+    }
+
+    public void hideImages(){
+        for(ImageView image : imageArray){
+            image.setVisibility(View.INVISIBLE);
+        }
 
     }
 }
